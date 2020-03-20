9e6343b75b5a9eec6dbb65ed12116279d2351f15
Mark Jordan <markybjordan@gmail.com>
2020-03-20 16:25:51 -0700

    Added a UILabel as a subview to the root view.

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-     
+        // label which displays click count
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        
     }
 
 
